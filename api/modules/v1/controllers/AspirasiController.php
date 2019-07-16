5d07085644e3d90de9a3def8209838ed7975432c
aldidoanta <aldidkurnia@gmail.com>
2019-07-16 16:33:02 +0700

    [Aspirasi] Add localized string for 403 error message

--- api/modules/v1/controllers/AspirasiController.php
+++ api/modules/v1/controllers/AspirasiController.php
@@ -275,7 +275,7 @@ class AspirasiController extends ActiveController
     public function checkAccess($action, $model = null, $params = [])
     {
         if (in_array($action, ['update', 'delete']) && $model->author_id !== Yii::$app->user->getId()) {
-            throw new ForbiddenHttpException();
+            throw new ForbiddenHttpException(Yii::t('app', 'error.role.permission'));
         }
     }
 
