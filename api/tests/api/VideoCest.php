b8cb37f72fee690254da3e965593ed349a437346
firman <firmanhxc@yahoo.co.id>
2019-07-29 09:28:22 +0700

    Unit test video : test list user

---
+++ api/tests/api/VideoCest.php
@@ -0,0 +1,130 @@
+<?php
+
+use Carbon\Carbon;
+
+class VideoCest
+{
+    public function _before(ApiTester $I)
+    {
+        Yii::$app->db->createCommand()->checkIntegrity(false)->execute();
+
+        Yii::$app->db->createCommand('TRUNCATE video_likes')->execute();
+        Yii::$app->db->createCommand('TRUNCATE videos')->execute();
+    }
+
+    public function getUserListTest(ApiTester $I)
+    {
+        $I->amUser('user');
+
+        $I->sendGET('/v1/video');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function getUserRWListTest(ApiTester $I)
+    {
+        $I->amUser('staffrw');
+
+        $I->sendGET('/v1/video');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function getUserListPublishedShowTest(ApiTester $I)
+    {
+        $I->haveInDatabase('videos', [
+            'id' => 1,
+            'category_id' => 22,
+            'title' => 'Lorem ipsum.',
+            'source' => 'youtube',
+            'video_url' => 'https://www.youtube.com/watch?v=YvG6D0qJflk',
+            'kabkota_id' => null,
+            'total_likes' => 0,
+            'seq' => 0,
+            'status' => 10,
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => 1557803314,
+            'updated_at' => 1557803314,
+        ]);
+
+        $I->amUser('user');
+
+        $I->sendGET('/v1/video');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $data = $I->grabDataFromResponseByJsonPath('$.data.items[0]');
+
+        $I->assertEquals(1, $data[0]['id']);
+    }
+
+    public function getUserRWListPublishedShowTest(ApiTester $I)
+    {
+        $I->haveInDatabase('videos', [
+            'id' => 1,
+            'category_id' => 22,
+            'title' => 'Lorem ipsum.',
+            'source' => 'youtube',
+            'video_url' => 'https://www.youtube.com/watch?v=YvG6D0qJflk',
+            'kabkota_id' => null,
+            'total_likes' => 0,
+            'seq' => 0,
+            'status' => 10,
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => 1557803314,
+            'updated_at' => 1557803314,
+        ]);
+
+        $I->amUser('staffrw');
+
+        $I->sendGET('/v1/video');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $data = $I->grabDataFromResponseByJsonPath('$.data.items[0]');
+
+        $I->assertEquals(1, $data[0]['id']);
+    }
+
+    public function getAdminListPublishedShowTest(ApiTester $I)
+    {
+        $I->haveInDatabase('videos', [
+            'id' => 1,
+            'category_id' => 22,
+            'title' => 'Lorem ipsum.',
+            'source' => 'youtube',
+            'video_url' => 'https://www.youtube.com/watch?v=YvG6D0qJflk',
+            'kabkota_id' => null,
+            'total_likes' => 0,
+            'seq' => 0,
+            'status' => 10,
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => 1557803314,
+            'updated_at' => 1557803314,
+        ]);
+
+        $I->amStaff('admin');
+
+        $I->sendGET('/v1/video');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $data = $I->grabDataFromResponseByJsonPath('$.data.items[0]');
+
+        $I->assertEquals(1, $data[0]['id']);
+    }
+
+}
