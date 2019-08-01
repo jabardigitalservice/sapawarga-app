3148e4c323bb453fc130cc9beaf6d8153ff15d7d
aldidoanta <aldidkurnia@gmail.com>
2019-08-01 17:33:57 +0700

    [Survey] Add test case for search and filter [ci skip]

--- api/tests/api/SurveyCest.php
+++ api/tests/api/SurveyCest.php
@@ -100,6 +100,177 @@ class SurveyCest
         $I->assertEquals(1, $data[0]['id']);
     }
 
+    public function getAdminListSearchTitleTest(ApiTester $I)
+    {
+        $I->haveInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Jajak Pendapat',
+            'status'       => 0,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->haveInDatabase('survey', [
+            'id'           => 2,
+            'title'        => 'Survei',
+            'status'       => 0,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->amStaff();
+
+        $I->sendGET('/v1/survey?title=Survei');
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'title' => 'Survei',
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'title' => 'Jajak Pendapat',
+        ]);
+    }
+
+    public function getAdminListFilterStatusTest(ApiTester $I)
+    {
+        $I->haveInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Survei',
+            'status'       => 1,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->haveInDatabase('survey', [
+            'id'           => 2,
+            'title'        => 'Survei',
+            'status'       => 10,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->amStaff();
+
+        $I->sendGET('/v1/survey?status=10');
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'data' => [
+                'items' => [
+                    [
+                        'status' => 10,
+                    ],
+                ],
+            ],
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'data' => [
+                'items' => [
+                    [
+                        'status' => 1,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function getAdminListFilterCategoryTest(ApiTester $I)
+    {
+        $I->haveInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Survei',
+            'status'       => 10,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->haveInDatabase('survey', [
+            'id'           => 2,
+            'title'        => 'Survei',
+            'status'       => 10,
+            'category_id'  => 21,
+            'external_url' => 'http://google.com',
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->amStaff();
+
+        $I->sendGET('/v1/survey?category_id=20');
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'category_id' => '20',
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'category_id' => '21',
+        ]);
+    }
+
+    public function getAdminListFilterAreaTest(ApiTester $I)
+    {
+        $I->haveInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Survei',
+            'status'       => 10,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'kabkota_id'   => 22,
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->haveInDatabase('survey', [
+            'id'           => 2,
+            'title'        => 'Survei',
+            'status'       => 10,
+            'category_id'  => 20,
+            'external_url' => 'http://google.com',
+            'kabkota_id'   => 23,
+            'created_at'   => '1554706345',
+            'updated_at'   => '1554706345',
+        ]);
+
+        $I->amStaff();
+
+        $I->sendGET('/v1/survey?kabkota_id=22');
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'data' => [ 'items' => [ [ 'kabkota_id' => 22, ], ], ],
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'data' => [ 'items' => [ [ 'kabkota_id' => 23, ], ],
+            ],
+        ]);
+    }
+
 
     public function getUserListDeletedDontShowTest(ApiTester $I)
     {
