17040f85c855dd42342cfcbf41e2600e77a9c79f
firman <firmanhxc@yahoo.co.id>
2019-08-06 07:34:58 +0700

    Polling save created_by, updated_by on create update

--- api/models/Polling.php
+++ api/models/Polling.php
@@ -6,6 +6,7 @@ use app\components\ModelHelper;
 use app\validator\InputCleanValidator;
 use Jdsteam\Sapawarga\Behaviors\AreaBehavior;
 use Yii;
+use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
@@ -230,12 +231,12 @@ class Polling extends ActiveRecord
     {
         return [
             [
-                'class'              => TimestampBehavior::className(),
+                'class'              => TimestampBehavior::class,
                 'createdAtAttribute' => 'created_at',
                 'updatedAtAttribute' => 'updated_at',
                 'value'              => time(),
             ],
-            AreaBehavior::class,
+            BlameableBehavior::class,
         ];
     }
 
