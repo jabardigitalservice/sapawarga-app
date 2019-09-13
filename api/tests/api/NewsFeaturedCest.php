cba41ff827fc7bbf388d51b6622e7a513b683812
Yoga Hanggara <yohang88@gmail.com>
2019-09-13 10:34:59 +0700

    [News] add api test
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- api/tests/api/NewsFeaturedCest.php
+++ api/tests/api/NewsFeaturedCest.php
@@ -8,6 +8,7 @@ class NewsFeaturedCest
 
         Yii::$app->db->createCommand('TRUNCATE news')->execute();
         Yii::$app->db->createCommand('TRUNCATE news_channels')->execute();
+        Yii::$app->db->createCommand('TRUNCATE news_featured')->execute();
 
         $I->haveInDatabase('news_channels', [
             'id'         => 1,
@@ -15,10 +16,8 @@ class NewsFeaturedCest
             'created_at' => '1554706345',
             'updated_at' => '1554706345',
         ]);
-    }
 
-    public function getUserListFeaturedActiveTest(ApiTester $I)
-    {
+        // PROVINSI
         // ACTIVE
         $I->haveInDatabase('news', [
             'id'          => 1,
@@ -37,7 +36,7 @@ class NewsFeaturedCest
 
         // DELETED
         $I->haveInDatabase('news', [
-            'id'          => 3,
+            'id'          => 2,
             'channel_id'  => 1,
             'title'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             'slug'        => 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit',
@@ -53,7 +52,7 @@ class NewsFeaturedCest
 
         // DISABLED
         $I->haveInDatabase('news', [
-            'id'          => 4,
+            'id'          => 3,
             'channel_id'  => 1,
             'title'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             'slug'        => 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit',
@@ -67,6 +66,72 @@ class NewsFeaturedCest
             'updated_at'  => '1554706345',
         ]);
 
+        // KABKOTA
+        // ACTIVE
+        $I->haveInDatabase('news', [
+            'id'          => 4,
+            'channel_id'  => 1,
+            'title'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'slug'        => 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit',
+            'content'     => 'Maecenas porttitor suscipit ex vitae hendrerit. Nunc sollicitudin quam et libero fringilla, eget varius nunc hendrerit.',
+            'kabkota_id'  => 22,
+            'featured'    => false,
+            'source_date' => '2019-06-20',
+            'source_url'  => 'https://google.com',
+            'cover_path'  => 'covers/test.jpg',
+            'status'      => 10,
+            'created_at'  => '1554706345',
+            'updated_at'  => '1554706345',
+        ]);
+
+        // DELETED
+        $I->haveInDatabase('news', [
+            'id'          => 5,
+            'channel_id'  => 1,
+            'title'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'slug'        => 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit',
+            'content'     => 'Maecenas porttitor suscipit ex vitae hendrerit. Nunc sollicitudin quam et libero fringilla, eget varius nunc hendrerit.',
+            'kabkota_id'  => 22,
+            'featured'    => false,
+            'source_date' => '2019-06-20',
+            'source_url'  => 'https://google.com',
+            'cover_path'  => 'covers/test.jpg',
+            'status'      => -1,
+            'created_at'  => '1554706345',
+            'updated_at'  => '1554706345',
+        ]);
+
+        // DISABLED
+        $I->haveInDatabase('news', [
+            'id'          => 6,
+            'channel_id'  => 1,
+            'title'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'slug'        => 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit',
+            'content'     => 'Maecenas porttitor suscipit ex vitae hendrerit. Nunc sollicitudin quam et libero fringilla, eget varius nunc hendrerit.',
+            'kabkota_id'  => 22,
+            'featured'    => false,
+            'source_date' => '2019-06-20',
+            'source_url'  => 'https://google.com',
+            'cover_path'  => 'covers/test.jpg',
+            'status'      => 0,
+            'created_at'  => '1554706345',
+            'updated_at'  => '1554706345',
+        ]);
+
+        $I->haveInDatabase('news_featured', [
+            'news_id' => 1,
+            'seq'     => 1,
+        ]);
+
+        $I->haveInDatabase('news_featured', [
+            'news_id'    => 4,
+            'kabkota_id' => 22,
+            'seq'        => 1,
+        ]);
+    }
+
+    public function getUserListFeaturedProvinsiTest(ApiTester $I)
+    {
         $I->amUser('staffrw');
 
         $I->sendGET('/v1/news/featured');
@@ -78,15 +143,34 @@ class NewsFeaturedCest
             'status'  => 200,
         ]);
 
-        $I->seeHttpHeader('X-Pagination-Total-Count', 1);
+        $data = $I->grabDataFromResponseByJsonPath('$.data');
 
-        $data = $I->grabDataFromResponseByJsonPath('$.data.items[0]');
-
-        $I->assertEquals(1, $data[0]['id']);
+        $I->assertEquals(1, $data[0][0]['id']);
+        $I->assertNull($data[0][1]);
+        $I->assertNull($data[0][2]);
+        $I->assertNull($data[0][3]);
+        $I->assertNull($data[0][4]);
     }
 
     public function getUserListFeaturedKabkotaTest(ApiTester $I)
     {
-        //
+        $I->amUser('staffrw');
+
+        $I->sendGET('/v1/news/featured?kabkota_id=22');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $data = $I->grabDataFromResponseByJsonPath('$.data');
+
+        $I->assertEquals(4, $data[0][0]['id']);
+        $I->assertNull($data[0][1]);
+        $I->assertNull($data[0][2]);
+        $I->assertNull($data[0][3]);
+        $I->assertNull($data[0][4]);
     }
 }
