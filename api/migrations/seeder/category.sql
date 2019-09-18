ad861abe360751c668f53d9748ab66671dc18fd0
aldidoanta <aldidkurnia@gmail.com>
2019-09-18 17:31:48 +0700

    [Seeder] Add seed data for saberHoax username and newsHoax categories

--- api/migrations/seeder/category.sql
+++ api/migrations/seeder/category.sql
@@ -26,6 +26,7 @@ VALUES
     ('video','Pembangunan',NULL,10,1557803314,1557803314),
     ('video','Event',NULL,10,1557803314,1557803314),
     ('video','Lainnya',NULL,10,1557803314,1557803314),
-    ('newsHoax','Klarifikasi',NULL,10,1557803314,1557803314);
-    ('newsHoax','Disinformasi',NULL,10,1557803314,1557803314);
-    ('newsHoax','Fakta',NULL,10,1557803314,1557803314);
+    ('newsHoax','Berita',NULL,10,1557803314,1557803314),
+    ('newsHoax','Disinformasi',NULL,10,1557803314,1557803314),
+    ('newsHoax','Klarifikasi Fakta',NULL,10,1557803314,1557803314),
+    ('newsHoax','Misinformasi',NULL,10,1557803314,1557803314);
