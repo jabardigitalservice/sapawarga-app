2fc112704578b4e736ab851f9fce17fbac79f16c
firman <firmanhxc@yahoo.co.id>
2019-09-20 14:05:09 +0700

    Update query only show created_by role staffprov

--- api/models/DashboardPolling.php
+++ api/models/DashboardPolling.php
@@ -20,12 +20,15 @@ class DashboardPolling extends Polling
     public function getPollingLatest($params)
     {
         $paramsSql[':status_published'] = Polling::STATUS_PUBLISHED;
+        $paramsSql[':role_staff_prov'] = User::ROLE_STAFF_PROV;
         $limit = Arr::get($params, 'limit', 10);
 
         $sql = 'SELECT p.id, p.category_id, c.name AS category_name, p.name, p.question, p.start_date, p.end_date, p.status
                 FROM polling p
                 LEFT JOIN categories c ON c.id = p.category_id
+                LEFT JOIN user u ON u.id = p.created_by
                 WHERE p.status = :status_published
+                AND u.role = :role_staff_prov
                 ORDER BY p.created_at DESC';
 
         $provider = new SqlDataProvider([
