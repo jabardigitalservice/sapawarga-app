4a89e1f2a75f089e5c5f2ba7a5025894cac05efe
Yoga Hanggara <yohang88@gmail.com>
2019-10-14 15:44:16 +0700

    [Import User] Import CSV, validate model

---
+++ api/models/UserImport.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * User Import
+ */
+class UserImport extends Model
+{
+    public $username;
+    public $password;
+    public $email;
+    public $name;
+    public $phone;
+    public $address;
+    public $rt;
+    public $rw;
+//    public $kel_id;
+//    public $kec_id;
+//    public $kabkota_id;
+    public $role;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'string', 'length' => [4, 255]],
+            [
+                'username',
+                'match',
+                'pattern' => '/^[a-z0-9_.]{4,255}$/',
+                'message' => Yii::t('app', 'error.username.pattern')
+            ],
+//            [
+//                'username',
+//                'unique',
+//                'targetClass' => '\app\models\User',
+//                'message' => Yii::t('app', 'error.username.taken'),
+//                'filter' => function ($query) {
+//                    $query->andWhere(['!=', 'id', $this->id]);
+//                }
+//            ],
+            ['email', 'trim'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => User::MAX_LENGTH],
+//            [
+//                'email',
+//                'unique',
+//                'targetClass' => '\app\models\User',
+//                'message' => Yii::t('app', 'error.email.taken'),
+//                'filter' => function ($query) {
+//                    $query->andWhere(['!=', 'id', $this->id]);
+//                }
+//            ],
+            ['password', 'string', 'length' => [5, User::MAX_LENGTH]],
+            [['name', 'address'], 'string', 'max' => User::MAX_LENGTH],
+            ['phone', 'string', 'length' => [3, 13]],
+        ];
+    }
+}
--- api/packages/jdsteam/sapawarga/src/Jobs/ImportUserJob.php
+++ api/packages/jdsteam/sapawarga/src/Jobs/ImportUserJob.php
@@ -2,6 +2,9 @@
 
 namespace Jdsteam\Sapawarga\Jobs;
 
+use app\models\UserImport;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use yii\base\BaseObject;
 use yii\queue\JobInterface;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
@@ -15,14 +18,51 @@ class ImportUserJob extends BaseObject implements JobInterface
         $reader = ReaderEntityFactory::createCSVReader();
         $reader->open($this->file);
 
+        $importedCollection = new Collection();
+        $rowNum = 0;
+
         foreach ($reader->getSheetIterator() as $sheet) {
             foreach ($sheet->getRowIterator() as $row) {
+                $rowNum++;
+
+                if ($rowNum === 1) {
+                    continue;
+                }
+
                 $cells = $row->getCells();
 
-                var_dump($cells); exit;
+                $importedRow = [
+                    'username'  => $cells[0]->getValue(),
+                    'email'     => $cells[1]->getValue(),
+                    'password'  => $cells[2]->getValue(),
+                    'role'      => $cells[3]->getValue(),
+                    'name'      => $cells[4]->getValue(),
+                    'phone'     => $cells[5]->getValue(),
+                    'address'   => $cells[6]->getValue(),
+                    'rt'        => $cells[7]->getValue(),
+                    'rw'        => $cells[8]->getValue(),
+                    'kabkota'   => $cells[9]->getValue(),
+                    'kecamatan' => $cells[10]->getValue(),
+                    'kelurahan' => $cells[11]->getValue(),
+                ];
+
+                $importedRow = Arr::except($importedRow, ['kabkota', 'kecamatan', 'kelurahan']);
+
+                $model = new UserImport();
+                $model->load($importedRow, '');
+
+                if ($model->validate() === false) {
+                    exit('Invalid data input.');
+                }
+
+                $importedCollection->push($model);
+
+                echo "Row: $rowNum\n";
             }
         }
 
         $reader->close();
+
+        // var_dump($importedCollection->take(5)); exit;
     }
 }
