0f15d04417c805047bac94d161819c118322b8f5
Yoga Hanggara <yohang88@gmail.com>
2019-10-30 09:52:23 +0700

    [Import User] Validate input area

--- api/models/UserImport.php
+++ api/models/UserImport.php
@@ -28,30 +28,7 @@ class UserImport extends Model
      */
     public function rules()
     {
-        return [
-            ['username', 'trim'],
-            ['username', 'string', 'length' => [4, 255]],
-            [
-                'username',
-                'match',
-                'pattern' => '/^[a-z0-9_.]{4,255}$/',
-                'message' => Yii::t('app', 'error.username.pattern')
-            ],
-            [
-                'username',
-                'unique',
-                'targetClass' => User::class,
-                'message' => Yii::t('app', 'error.username.taken'),
-            ],
-            ['email', 'trim'],
-            ['email', 'email'],
-            ['email', 'string', 'max' => User::MAX_LENGTH],
-            [
-                'email',
-                'unique',
-                'targetClass' => User::class,
-                'message' => Yii::t('app', 'error.email.taken'),
-            ],
+        $rules = [
             ['password', 'string', 'length' => [5, User::MAX_LENGTH]],
             [['name', 'address'], 'string', 'max' => User::MAX_LENGTH],
             ['phone', 'string', 'length' => [3, 13]],
@@ -70,6 +47,43 @@ class UserImport extends Model
             ['rw', 'required', 'when' => function ($model) {
                 return $model->role <= User::ROLE_STAFF_RW;
             }],
+        ];
+
+        return array_merge($rules, $this->rulesUsername(), $this->rulesEmail());
+    }
+
+    protected function rulesUsername()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'string', 'length' => [4, 255]],
+            [
+                'username',
+                'match',
+                'pattern' => '/^[a-z0-9_.]{4,255}$/',
+                'message' => Yii::t('app', 'error.username.pattern')
+            ],
+            [
+                'username',
+                'unique',
+                'targetClass' => User::class,
+                'message' => Yii::t('app', 'error.username.taken'),
+            ],
+        ];
+    }
+
+    protected function rulesEmail()
+    {
+        return [
+            ['email', 'trim'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => User::MAX_LENGTH],
+            [
+                'email',
+                'unique',
+                'targetClass' => User::class,
+                'message' => Yii::t('app', 'error.email.taken'),
+            ],
         ];
     }
 }
