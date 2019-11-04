d302704f35a87d3dac770c379df6ff3f0dad2418
Yoga Hanggara <yohang88@gmail.com>
2019-11-04 14:21:47 +0700

    [Profile] List education

--- api/modules/v1/controllers/EducationLevelController.php
+++ api/modules/v1/controllers/EducationLevelController.php
@@ -50,19 +50,7 @@ class EducationLevelController extends ActiveController
     public function actionIndex()
     {
         return [
-            'items' => [
-                ['id' => 1, 'name' => 'Tidak Ada', 'seq' => 0],
-                ['id' => 2, 'name' => 'SD/MI', 'seq' => 1],
-                ['id' => 3, 'name' => 'SMP/MTS', 'seq' => 1],
-                ['id' => 4, 'name' => 'SMA/SMK', 'seq' => 1],
-                ['id' => 5, 'name' => 'D1', 'seq' => 1],
-                ['id' => 6, 'name' => 'D2', 'seq' => 1],
-                ['id' => 7, 'name' => 'D3', 'seq' => 1],
-                ['id' => 8, 'name' => 'S1', 'seq' => 1],
-                ['id' => 9, 'name' => 'S2', 'seq' => 1],
-                ['id' => 10, 'name' => 'S3', 'seq' => 1],
-                ['id' => 11, 'name' => 'Lainnya', 'seq' => 99],
-            ],
+            'items' => include __DIR__ . '/../../../config/references/education_level.php',
         ];
     }
 }
