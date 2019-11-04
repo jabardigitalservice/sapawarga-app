d50f58c0b19ffb4f66a6a8b82d3d36d1ebb7f6e8
Yoga Hanggara <yohang88@gmail.com>
2019-11-04 14:30:02 +0700

    [Profile] add FK

--- api/migrations/m191104_042934_add_user_job_type_education_level.php
+++ api/migrations/m191104_042934_add_user_job_type_education_level.php
@@ -11,8 +11,17 @@ class m191104_042934_add_user_job_type_education_level extends CustomMigration
      */
     public function safeUp()
     {
-        $this->addColumn('user', 'job_type_id', $this->smallInteger()->unsigned()->null()->after('address'));
-        $this->addColumn('user', 'education_level_id', $this->smallInteger()->unsigned()->null()->after('job_type_id'));
+        $this->addColumn('user', 'job_type_id', $this->integer()->null()->after('address'));
+        $this->addColumn('user', 'education_level_id', $this->integer()->null()->after('job_type_id'));
+
+        $this->addForeignKey(
+            'fk-user-job_type_id',
+            'user',
+            'job_type_id',
+            'job_types',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
@@ -20,6 +29,8 @@ class m191104_042934_add_user_job_type_education_level extends CustomMigration
      */
     public function safeDown()
     {
+        $this->dropForeignKey('fk-user-job_type_id', 'user');
+
         $this->dropColumn('user', 'education_level_id');
         $this->dropColumn('user', 'job_type_id');
     }
