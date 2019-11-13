1025c3a479684475f348941bdb0e80b4b93fe5d7
Yoga Hanggara <yohang88@gmail.com>
2019-11-13 19:45:57 +0700

    #248 refactor to new class, new methods, get roles from database (instead hardcoded)

---
+++ api/modules/v1/repositories/UserRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace app\modules\v1\repositories;
+
+use Yii;
+use app\models\User;
+
+class UserRepository
+{
+    public function getUsersCountAllRolesByArea($selectedRoles, $kabKotaId, $kecId, $kelId): array
+    {
+        $availableRoles = Yii::$app->authManager->getRoles();
+
+        $items = [];
+        $index = 1;
+
+        foreach ($availableRoles as $role) {
+            if (in_array($role->name, $selectedRoles, true) === false) {
+                continue;
+            }
+
+            $items[] = [
+                'id'    => $index,
+                'level' => $role->name,
+                'name'  => $role->description,
+                'value' => $this->getUsersCountRoleByArea($role->name, $kabKotaId, $kecId, $kelId),
+            ];
+
+            $index++;
+        }
+
+        return $items;
+    }
+
+    public function getUsersCountRoleByArea($roleName, $kabKotaId, $kecId, $kelId): int
+    {
+        $userIds = Yii::$app->authManager->getUserIdsByRole($roleName);
+
+        $query = User::find()->where(['in', 'id', $userIds]);
+
+        if ($kabKotaId) {
+            $query->andWhere(['kabkota_id' => $kabKotaId]);
+        }
+
+        if ($kecId) {
+            $query->andWhere(['kec_id' => $kecId]);
+        }
+
+        if ($kelId) {
+            $query->andWhere(['kel_id' => $kelId]);
+        }
+
+        $query->andWhere(['status' => User::STATUS_ACTIVE]);
+
+        return $query->count();
+    }
+}
