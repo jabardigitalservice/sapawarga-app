10f53ba0ae8abd3ffe8d03b5acdfe40d17cc7c1f
Yoga Hanggara <yohang88@gmail.com>
2019-11-15 16:19:31 +0700

    #225 add more test cases

---
+++ api/tests/api/StaftPermissionsCest.php
@@ -0,0 +1,121 @@
+<?php
+
+class StaftPermissionsCest
+{
+    public function _before(ApiTester $I)
+    {
+        Yii::$app->db->createCommand()->checkIntegrity(false)->execute();
+
+        Yii::$app->db->createCommand('TRUNCATE auth_assignment')->execute();
+        Yii::$app->db->createCommand('TRUNCATE user')->execute();
+
+        $sql = file_get_contents(__DIR__ . '/../../migrations/seeder/user_and_permission.sql');
+        Yii::$app->db->createCommand($sql)->execute();
+    }
+
+    // VIEW PERMISSION
+    public function staffProvCanViewCascaded(ApiTester $I)
+    {
+        $I->amStaff('staffprov');
+
+        $I->sendGET('/v1/staff/2'); // staffprov
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/3'); // staffkabkota
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/4'); // staffkabkota2
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/5'); // staffkec
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/6'); // staffkec2
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/7'); // staffkec3
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/8'); // staffkec4
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/9'); // staffkel
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/17'); // staffrw
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendGET('/v1/staff/18'); // staffrw
+        $I->canSeeResponseCodeIs(200);
+    }
+
+    public function staffProvCanEditCascaded(ApiTester $I)
+    {
+        $I->amStaff('staffprov');
+
+        $I->sendPUT('/v1/staff/2', ['name' => 'test']); // staffprov
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/3', ['name' => 'test']); // staffkabkota
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/4', ['name' => 'test']); // staffkabkota2
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/5', ['name' => 'test']); // staffkec
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/6', ['name' => 'test']); // staffkec2
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/7', ['name' => 'test']); // staffkec3
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/8', ['name' => 'test']); // staffkec4
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/9', ['name' => 'test']); // staffkel
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/17', ['name' => 'test']); // staffrw
+        $I->canSeeResponseCodeIs(200);
+
+        $I->sendPUT('/v1/staff/18', ['name' => 'test']); // staffrw
+        $I->canSeeResponseCodeIs(200);
+    }
+
+    public function staffProvCanDeleteCascaded(ApiTester $I)
+    {
+        $I->amStaff('staffprov');
+
+        $I->sendDELETE('/v1/staff/2'); // staffprov
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/3'); // staffkabkota
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/4'); // staffkabkota2
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/5'); // staffkec
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/6'); // staffkec2
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/7'); // staffkec3
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/8'); // staffkec4
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/9'); // staffkel
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/17'); // staffrw
+        $I->canSeeResponseCodeIs(204);
+
+        $I->sendDELETE('/v1/staff/18'); // staffrw
+        $I->canSeeResponseCodeIs(204);
+    }
+}
