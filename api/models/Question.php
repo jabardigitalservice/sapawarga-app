94aad3611f5e4822e91b7f5c314032b714ecf96c
firman <firmanhxc@yahoo.co.id>
2019-12-02 10:50:22 +0700

    Question and QuestionComment model

--- api/models/Question.php
+++ api/models/Question.php
@@ -2,15 +2,115 @@
 
 namespace app\models;
 
+use app\components\ModelHelper;
+use Jdsteam\Sapawarga\Models\Concerns\HasActiveStatus;
+use Jdsteam\Sapawarga\Models\Contracts\ActiveStatus;
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
-class Question extends ActiveRecord
+/**
+ * This is the model class for table "question".
+ *
+ * @property int $id
+ * @property string $question
+ * @property int $status
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_by
+ */
+class Question extends ActiveRecord implements ActiveStatus
 {
+    use HasActiveStatus;
+
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'releases'; // TODO
+        return 'questions';
+    }
+
+    public function getComment()
+    {
+        return $this->hasMany(QuestionComment::class, ['id' => 'question_id']);
+    }
+
+    public function getAuthor()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['question', 'string', 'max' => 255],
+
+            [['question'], 'trim'],
+            [['question'], 'safe'],
+			[['question'],'required'],
+
+            ['status', 'integer'],
+            ['status', 'in', 'range' => [-1, 0, 10]],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'question',
+            'total_comment',
+            'status',
+            'status_label' => 'StatusLabel',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'author' => 'AuthorField',
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'question' => 'Pertanyaan',
+            'status' => 'Status',
+        ];
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => time(),
+            ],
+            BlameableBehavior::class,
+        ];
+    }
+
+    protected function getAuthorField()
+    {
+        $publicBaseUrl = Yii::$app->params['storagePublicBaseUrl'];
+
+        return [
+            'id' => $this->author->id,
+            'name' => $this->author->name,
+            'photo_url_full' => $this->author->photo_url ? "$publicBaseUrl/{$this->author->photo_url}" : null,
+        ];
     }
 }
--- api/models/QuestionComment.php
+++ api/models/QuestionComment.php
@@ -2,15 +2,103 @@
 
 namespace app\models;
 
+use app\components\ModelHelper;
+use Jdsteam\Sapawarga\Models\Concerns\HasActiveStatus;
+use Jdsteam\Sapawarga\Models\Contracts\ActiveStatus;
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
-class QuestionComment extends ActiveRecord
+/**
+ * This is the model class for table "question_comment".
+ *
+ * @property int $id
+ * @property int $question_id
+ * @property string $comment
+ * @property int $status
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_by
+ */
+class QuestionComment extends ActiveRecord implements ActiveStatus
 {
+    use HasActiveStatus;
+
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'areas'; // TODO
+        return 'question_comment';
+    }
+
+    public function getChannel()
+    {
+        return $this->hasOne(Question::class, ['id' => 'channel_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['comment', 'string', 'max' => 500],
+            ['comment', 'string', 'min' => 10],
+
+            [['comment'], 'trim'],
+            [['comment'], 'safe'],
+
+            [['question_id', 'comment', 'status'], 'required' ],
+
+            ['status', 'integer'],
+            ['status', 'in', 'range' => [-1, 0, 10]],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'question_id',
+            'comment',
+            'status',
+            'status_label' => 'StatusLabel',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'author' => 'AuthorField',
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'comment' => 'Komentar',
+            'channel_id' => 'Website',
+            'status' => 'Status',
+        ];
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class'              => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value'              => time(),
+            ],
+            BlameableBehavior::class,
+        ];
     }
 }
