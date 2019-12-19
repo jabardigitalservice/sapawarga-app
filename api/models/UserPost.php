98acb8ba37c810a961283a4d474521ca54653e32
firman <firmanhxc@yahoo.co.id>
2019-12-19 13:23:55 +0700

    User post created, update, list, and like

---
+++ api/models/UserPost.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace app\models;
+
+use app\components\ModelHelper;
+use Jdsteam\Sapawarga\Models\Concerns\HasActiveStatus;
+use Jdsteam\Sapawarga\Models\Contracts\ActiveStatus;
+use Yii;
+use yii\behaviors\AttributeTypecastBehavior;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $text
+ * @property string $image_path
+ * @property int $status
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_by
+ */
+class UserPost extends ActiveRecord implements ActiveStatus
+{
+    use HasActiveStatus;
+
+    public $likes_count;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_posts';
+    }
+
+    public function getAuthor()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    public function getComments()
+    {
+        return $this->hasMany(UserPostComment::class, ['user_post_id' => 'id']);
+    }
+
+    public function getLikes()
+    {
+        return $this->hasMany(Like::class, ['entity_id' => 'id'])
+                    ->andOnCondition(['type' => Like::TYPE_USER_POST]);
+    }
+
+    public function getLastComment()
+    {
+        return UserPostComment::findOne($this->last_user_post_id);
+    }
+
+    public function getIsUserLiked()
+    {
+        $isLiked = Like::find()
+            ->where(['entity_id' => $this->id])
+            ->andWhere(['type' => Like::TYPE_USER_POST])
+            ->andWhere(['user_id' => Yii::$app->user->id])
+            ->count();
+
+        if ($isLiked > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['text', 'string', 'max' => 1000],
+            ['text', 'string', 'min' => 10],
+
+            [['text'], 'trim'],
+            [['text'], 'safe'],
+            [['text', 'status', 'image_path'],'required'],
+
+            [['status', 'last_user_post_id'], 'integer'],
+            ['status', 'in', 'range' => [-1, 0, 10]],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'text',
+            'image_path_full' => function () {
+                $publicBaseUrl = Yii::$app->params['storagePublicBaseUrl'];
+                return "{$publicBaseUrl}/{$this->image_path}";
+            },
+            'likes_count',
+            'comments_count' => 'CommentsCount',
+            'last_user_post_id',
+            'last_comment' => 'lastComment',
+            'status',
+            'status_label' => 'StatusLabel',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'is_liked' => 'IsUserLiked',
+            'user' => 'AuthorField',
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'text' => 'Deskripsi',
+            'image_path' => 'Photo',
+            'status' => 'Status',
+        ];
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => time(),
+            ],
+            'typecast' => [
+                'class' => AttributeTypecastBehavior::class,
+                'attributeTypes' => [
+                    'likes_count' => AttributeTypecastBehavior::TYPE_INTEGER,
+                ],
+                'typecastAfterValidate' => false,
+                'typecastBeforeSave' => false,
+                'typecastAfterFind' => true,
+            ],
+            BlameableBehavior::class,
+        ];
+    }
+
+    protected function getCommentsCount()
+    {
+        return (int)$this->getComments()->count();
+    }
+
+    protected function getAuthorField()
+    {
+        $publicBaseUrl = Yii::$app->params['storagePublicBaseUrl'];
+
+        return [
+            'id' => $this->author->id,
+            'name' => $this->author->name,
+            'photo_url_full' => $this->author->photo_url ? "$publicBaseUrl/{$this->author->photo_url}" : null,
+            'role_label' => $this->author->getRoleName(),
+        ];
+    }
+}
---
+++ api/modules/v1/controllers/UserPostController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use app\models\UserPost;
+use app\models\UserPostSearch;
+use app\models\Like;
+use yii\filters\AccessControl;
+use Illuminate\Support\Arr;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\web\NotFoundHttpException;
+use yii\web\ForbiddenHttpException;
+use app\modules\v1\repositories\UserPostRepository;
+
+class UserPostController extends ActiveController
+{
+    public $modelClass = UserPost::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::className(),
+            'actions' => [
+                'index' => ['get'],
+                'view' => ['get'],
+                'create' => ['post'],
+                'update' => ['put'],
+                'delete' => ['delete'],
+            ],
+        ];
+
+        return $this->behaviorCors($behaviors);
+    }
+
+    protected function behaviorAccess($behaviors)
+    {
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'only' => ['index', 'view', 'create', 'update', 'delete'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['index', 'view', 'create', 'update', 'delete'],
+                    'roles' => ['admin', 'staffProv', 'staffRW'],
+                ],
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        // Override Actions
+        unset($actions['view']);
+        unset($actions['delete']);
+
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
+
+     /**
+     * @param $id
+     * @return mixed|UserPost
+     * @throws \yii\web\NotFoundHttpException
+     */
+    public function actionView($id)
+    {
+        $repository = new UserPostRepository();
+        $getDetail = $repository->getDetail($id);
+
+        if ($getDetail === null) {
+            throw new NotFoundHttpException("Object not found: $id");
+        }
+
+        return $getDetail;
+    }
+
+    /**
+     * Delete entity with soft delete / status flagging
+     *
+     * @param $id
+     * @return string
+     * @throws \yii\web\ForbiddenHttpException
+     * @throws \yii\web\NotFoundHttpException
+     * @throws \yii\web\ServerErrorHttpException
+     */
+    public function actionDelete($id)
+    {
+        $model = $this->findModel($id, $this->modelClass);
+
+        $this->checkAccess('delete', $model, $id);
+
+        return $this->applySoftDelete($model);
+    }
+
+    /**
+     * Delete entity with soft delete / status flagging
+     *
+     * @param $id
+     */
+    public function actionLikes($id)
+    {
+        $userId = Yii::$app->user->getId();
+        $userLike = Like::find()->where(['entity_id' => $id])
+            ->andWhere(['type' => Like::TYPE_USER_POST])
+            ->andWhere(['user_id' => $userId])
+            ->one();
+
+        if (! empty($userLike)) {
+            $unlike = Like::findOne($userLike->id);
+            $unlike->delete();
+        } else {
+            $like = new Like();
+            $like->entity_id = $id;
+            $like->user_id = $userId;
+            $like->type = Like::TYPE_USER_POST;
+            $like->save();
+        }
+
+        $response = Yii::$app->getResponse();
+        $response->setStatusCode(200);
+
+        return 'ok';
+    }
+
+    /**
+     * Checks the privilege of the current user.
+     * throw ForbiddenHttpException if access should be denied
+     *
+     * @param string $action the ID of the action to be executed
+     * @param object $model the model to be accessed. If null, it means no specific model is being accessed.
+     * @param array $params additional parameters
+     */
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        $authUser = Yii::$app->user;
+        $authUserId = $authUser->id;
+
+        // Admin can do everything
+        if ($authUser->can('admin')) {
+            return true;
+        }
+
+        if ($action === 'update' || $action === 'delete') {
+            if ($model->created_by !== \Yii::$app->user->id) {
+                throw new ForbiddenHttpException(Yii::t('app', 'error.role.permission'));
+            }
+        }
+    }
+
+    public function prepareDataProvider()
+    {
+        $params = Yii::$app->request->getQueryParams();
+
+        $user = Yii::$app->user;
+        $search = new UserPostSearch();
+        if ($user->can('staffRW')) {
+            $search->scenario = UserPostSearch::SCENARIO_LIST_USER;
+        }
+
+        return $search->search($params);
+    }
+}
