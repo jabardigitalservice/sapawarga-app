180765a0cef9ee214a499fd83add0e5cd40f9a56
Yoga Hanggara <yohang88@gmail.com>
2020-02-06 11:04:59 +0700

    log activity for last activity

--- api/packages/jdsteam/sapawarga/src/Filters/RecordLastActivity.php
+++ api/packages/jdsteam/sapawarga/src/Filters/RecordLastActivity.php
@@ -3,6 +3,7 @@
 namespace Jdsteam\Sapawarga\Filters;
 
 use app\models\User;
+use Monolog\Logger;
 use Yii;
 use yii\base\ActionFilter;
 use yii\db\Expression;
@@ -19,6 +20,32 @@ class RecordLastActivity extends ActionFilter
             $user->save(false);
         }
 
+        $this->logInfo();
+
         return parent::afterAction($action, $result);
+    }
+
+    protected function logInfo()
+    {
+        $user = Yii::$app->user->identity;
+
+        /**
+         * @var Logger $logger
+         */
+        $monologComponent = Yii::$app->monolog;
+        $logger = $monologComponent->getLogger('main');
+
+        $logger->info(
+            'USER_LAST_ACTIVITY',
+            [
+                'user_id'    => $user->id,
+                'username'   => $user->username,
+                'kabkota_id' => $user->kabkota_id ? (int) $user->kabkota_id : null,
+                'kec_id'     => $user->kec_id ? (int) $user->kec_id : null,
+                'kel_id'     => $user->kel_id ? (int) $user->kel_id : null,
+                'role'       => (int) $user->role,
+                'status'     => $user->status,
+            ]
+        );
     }
 }
