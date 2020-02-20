07ab3c0b2182d6f2b89d1f2d573362250f231a0d
firman <firmanhxc@yahoo.co.id>
2020-02-20 16:33:06 +0700

    User post push notif

--- api/models/UserPostComment.php
+++ api/models/UserPostComment.php
@@ -9,6 +9,7 @@ use yii\behaviors\AttributeTypecastBehavior;
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use app\components\ModelHelper;
 
 /**
  * This is the model class for table "user_post_comments".
@@ -119,16 +120,46 @@ class UserPostComment extends ActiveRecord implements ActiveStatus
     public function afterSave($insert, $changedAttributes)
     {
         if ($insert) {
+            // Save the last comment id and update comments_count
             $commentsCount = UserPostComment::find()
                 ->where(['user_post_id' => $this->user_post_id])
                 ->andWhere(['status' => UserPostComment::STATUS_ACTIVE])
                 ->count();
 
-            // Save the last comment id + update comments_count
             $this->userPost->last_user_post_comment_id = $this->id;
             $this->userPost->comments_count = $commentsCount;
             $this->userPost->save(false);
+
+            // Send push notif
+            if (!YII_ENV_TEST) {
+                $this->sendNotification($this->userPost->created_by, $this->created_by);
+            }
         }
         return parent::afterSave($insert, $changedAttributes);
+    }
+
+    protected function sendNotification($userIdPost, $userIdComment)
+    {
+        $userIdPost = User::findIdentity($userIdPost);
+        $userIdComment = User::findIdentity($userIdComment);
+
+        $payload = [
+            'categoryName'  => Notification::CATEGORY_LABEL_USER_POST,
+            'title'         => "{$userIdComment->name} mengomentari kegiatan anda",
+            'description'   => null,
+            'target'        => [
+                'kabkota_id'    => $userIdPost->kabkota_id,
+                'kec_id'        => $userIdPost->kec_id,
+                'kel_id'        => $userIdPost->kel_id,
+                'rw'            => $userIdPost->rw,
+                'push_token'    => $userIdPost->push_token,
+            ],
+            'meta'          => [
+                'target'    => 'user-post',
+                'id'        => $this->user_post_id,
+            ],
+        ];
+
+        ModelHelper::sendNewContentNotification($payload);
     }
 }
