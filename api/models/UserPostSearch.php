29f7e41fa08d13bfcec21a283fdb9a289258b9c8
firman <firmanhxc@yahoo.co.id>
2020-02-28 09:10:57 +0700

    Fix sorting user post

--- api/models/UserPostSearch.php
+++ api/models/UserPostSearch.php
@@ -77,6 +77,7 @@ class UserPostSearch extends UserPost
                     'status',
                     'likes_count',
                     'comments_count',
+                    'user.name',
                 ],
             ],
             'pagination' => [
