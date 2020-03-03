1ce052dcddf2067ecc9026fee6f7c39ea0b3d3e8
firman <firmanhxc@yahoo.co.id>
2020-03-03 14:51:45 +0700

    Add comment migrate

--- api/migrations/m200302_084508_update_user_post_add_images_path.php
+++ api/migrations/m200302_084508_update_user_post_add_images_path.php
@@ -13,6 +13,7 @@ class m200302_084508_update_user_post_add_images_path extends CustomMigration
     {
         $this->addColumn('user_posts', 'images', $this->json()->after('image_path'));
 
+        // Migrate old image from string format to json format
         \Yii::$app->db->createCommand('
                 UPDATE user_posts
                 set images = COALESCE(CONCAT(\'[{"path":"\', image_path, \'"}]\'), null)
