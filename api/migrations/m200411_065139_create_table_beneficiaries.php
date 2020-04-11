20e0f4b2839690e7f6d00e3db28815beba1da911
firmansyah <firmanhxc@yahoo.co.id>
2020-04-11 17:40:28 +0700

    Migrate table, Model Beneficiary

---
+++ api/migrations/m200411_065139_create_table_beneficiaries.php
@@ -0,0 +1,52 @@
+<?php
+
+use app\components\CustomMigration;
+
+/**
+ * Class m200411_065139_create_table_beneficiaries */
+class m200411_065139_create_table_beneficiaries extends CustomMigration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('beneficiaries', [
+            'id' => $this->primaryKey(),
+            'nik' => $this->string(),
+            'no_kk' => $this->string(),
+            'name' => $this->string()->notNull(),
+            'kabkota_bps_id' => $this->string(),
+            'kec_bps_id' => $this->string(),
+            'kel_bps_id' => $this->string(),
+            'rt' => $this->string(),
+            'rw' => $this->string(),
+            'address' => $this->string(),
+            'phone' => $this->string(),
+            'total_family_members' => $this->integer(),
+            'job_type_name' => $this->string(),
+            'job_status' => $this->string(),
+            'income_before' => $this->integer(),
+            'income_after' => $this->integer(),
+            'image_ktp' => $this->string(),
+            'image_kk' => $this->string(),
+            'status_verification' => $this->integer()->notNull(),
+            'status' => $this->integer()->notNull(),
+            'info' => $this->string(),
+
+            'created_by' => $this->integer(),
+            'updated_by' => $this->integer(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('beneficiaries');
+    }
+
+}
---
+++ api/models/Beneficiary.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace app\models;
+
+use app\components\ModelHelper;
+use app\validator\InputCleanValidator;
+use Jdsteam\Sapawarga\Models\Concerns\HasActiveStatus;
+use Jdsteam\Sapawarga\Models\Contracts\ActiveStatus;
+use Yii;
+use yii\behaviors\AttributeTypecastBehavior;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "beneficiaries".
+ *
+ * @property int $id
+ * @property string $nik
+ * @property string $name
+ * @property string $kabkota_bps_id
+ * @property string $kec_bps_id
+ * @property string $kel_bps_id
+ * @property string $rt
+ * @property string $rw
+ * @property string $address
+ * @property string $phone
+ * @property int $total_family_members
+ * @property string $job_type_name
+ * @property string $job_status
+ * @property int $income_before
+ * @property int $income_after
+ * @property string $image_ktp
+ * @property string $image_kk
+ * @property int $status_verification
+ * @property int $status
+ * @property string $info
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_at
+ */
+
+class Beneficiary extends ActiveRecord implements ActiveStatus
+{
+    use HasActiveStatus;
+
+    const STATUS_PUBLISHED = 10;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'beneficiaries';
+    }
+
+    public function getKabkota()
+    {
+        return $this->hasOne(Area::className(), ['id' => 'kabkota_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [
+                ['nik', 'name', 'status_verification', 'status'],
+                'required',
+            ],
+
+            [['nik'], 'unique'],
+
+            [
+                ['name', 'address', 'phone', 'no_kk', 'job_type_name', 'job_status', 'info', 'image_ktp', 'image_kk', 'rt', 'rw'],
+                'trim'
+            ],
+
+            [
+                ['status_verification', 'status', 'kabkota_bps_id', 'kec_bps_id', 'kel_bps_id', 'income_before', 'income_after', 'total_family_members'],
+                'integer'
+            ],
+
+            ['status_verification', 'in', 'range' => [1, 2, 3]],
+            ['status', 'in', 'range' => [-1, 0, 10]],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'nik',
+            'no_kk',
+            'name',
+            'kabkota_bps_id',
+            'kec_bps_id',
+            'kel_bps_id',
+            'rt',
+            'rw',
+            'address',
+            'phone',
+            'total_family_members',
+            'job_type_name',
+            'job_status',
+            'income_before',
+            'income_after',
+            'image_ktp_url' => function () {
+                $publicBaseUrl = Yii::$app->params['storagePublicBaseUrl'];
+                return $this->image_ktp ? "$publicBaseUrl/$this->image_ktp" : null;
+            },
+            'image_kk_url' => function () {
+                $publicBaseUrl = Yii::$app->params['storagePublicBaseUrl'];
+                return $this->image_kk ? "$publicBaseUrl/$this->image_kk" : null;
+            },
+            'info',
+            'status_verification',
+            'status',
+            'status_label' => 'StatusLabel',
+            'created_at',
+            'updated_at',
+            'created_by',
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'nik' => 'No KTP',
+            'no_kk' => 'No KK',
+            'name' => 'Nama Lengkap',
+            'kabkota_bps_id' => 'Kota',
+            'kec_bps_id' => 'Kecamatan',
+            'kel_bps_id' => 'Kelurahan / Desa',
+            'rt' => 'RT',
+            'rw' => 'RW',
+            'address' => 'Alamat',
+            'phone' => 'Telepon',
+            'total_family_members' => 'Total',
+            'job_type_name' => 'Lapangan Usaha',
+            'job_status' => 'Status Kedudukan',
+            'income_before' => 'Penghasilan Sebelum',
+            'income_after' => 'Penghasilan Sesudah',
+            'image_ktp' => 'Foto KTP',
+            'image_kk' => 'Foto KK',
+            'status_verification' => 'Status Vefifikasi',
+            'status' => '',
+            'info' => 'Keterangan',
+        ];
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class'              => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value'              => time(),
+            ],
+            BlameableBehavior::class,
+        ];
+    }
+
+}
