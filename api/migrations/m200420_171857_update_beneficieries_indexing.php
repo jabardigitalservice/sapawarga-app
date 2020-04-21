46f3c7583816651c975097a0a721db0ebf2a6381
firmansyah <firmanhxc@yahoo.co.id>
2020-04-21 21:56:00 +0700

    Beneficieries indexing, query optimize

---
+++ api/migrations/m200420_171857_update_beneficieries_indexing.php
@@ -0,0 +1,29 @@
+<?php
+
+use app\components\CustomMigration;
+
+/**
+ * Class m200420_171857_update_beneficieries_indexing */
+class m200420_171857_update_beneficieries_indexing extends CustomMigration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createIndex(
+            'idx-beneficiaries',
+            'beneficiaries',
+            ['nik' , 'domicile_kabkota_bps_id' , 'domicile_kec_bps_id' , 'domicile_kel_bps_id' , 'domicile_rw', 'status_verification', 'status']
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('idx-beneficiaries', 'beneficiaries');
+    }
+
+}
