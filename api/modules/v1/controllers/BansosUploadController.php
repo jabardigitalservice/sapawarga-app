638231a5fca7218c66f781b1e27e59c1d83c5a52
Yoga Hanggara <yohang88@gmail.com>
2020-04-28 16:17:18 +0700

    bansos upload document bnba
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ api/modules/v1/controllers/BansosUploadController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use app\models\Area;
+use creocoder\flysystem\Filesystem;
+use Yii;
+use yii\base\DynamicModel;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\NotFoundHttpException;
+use yii\web\UploadedFile;
+
+/**
+ * BansosUploadController implements the CRUD actions for Banner model.
+ */
+class BansosUploadController extends ActiveController
+{
+    public $modelClass = DynamicModel::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::class,
+            'actions' => [
+                'upload' => ['post'],
+            ],
+        ];
+
+        return $this->behaviorCors($behaviors);
+    }
+
+    protected function behaviorAccess($behaviors)
+    {
+        // setup access
+        $behaviors['access'] = [
+            'class' => AccessControl::class,
+            'only' => ['upload'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['upload'],
+                    'roles' => ['admin'],
+                ]
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        return $actions;
+    }
+
+    public function actionUpload()
+    {
+        /**
+         * @var Filesystem $filesystem
+         */
+        $filesystem = Yii::$app->fs;
+
+        $kabkotaId = Yii::$app->request->post('kabkota_id');
+        $kecId     = Yii::$app->request->post('kec_id');
+        $file      = UploadedFile::getInstanceByName('file');
+
+        $model = new DynamicModel(['file' => $file, 'kabkota_id' => $kabkotaId, 'kec_id' => $kecId]);
+
+        $model->addRule('file', 'required');
+        $model->addRule('kabkota_id', 'trim');
+        $model->addRule('kabkota_id', 'required');
+
+        if ($model->validate() === false) {
+            $response = Yii::$app->getResponse();
+            $response->setStatusCode(422);
+
+            return $model->getErrors();
+        }
+
+        $kabkota = Area::findOne(['id' => $kabkotaId]);
+
+        $ext          = $file->getExtension();
+        $date         = date('Ymd_His');
+        $relativePath = "/bansos-bnba/{$kabkota->code_bps}_{$date}.{$ext}";
+
+        $filesystem->write($relativePath, file_get_contents($file->tempName));
+
+        return ['file_path' => $relativePath];
+    }
+}
