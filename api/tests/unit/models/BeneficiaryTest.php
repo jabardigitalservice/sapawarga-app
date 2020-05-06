589d787df55f430daf1d986edd51d8d26e35f182
Aldi Doanta <aldidkurnia@gmail.com>
2020-05-06 14:38:12 +0700

    [Bansos] Add unit test for nik and total_family_members fields

--- api/tests/unit/models/BeneficiaryTest.php
+++ api/tests/unit/models/BeneficiaryTest.php
@@ -3,6 +3,8 @@
 namespace tests\unit\models;
 
 use app\models\Beneficiary;
+use app\validator\NikValidator;
+use yii\base\DynamicModel;
 
 class BeneficiaryTest extends \Codeception\Test\Unit
 {
@@ -54,5 +56,58 @@ class BeneficiaryTest extends \Codeception\Test\Unit
         $model->domicile_rt = 1;
         $model->validate();
         $this->assertEquals('1', $model->domicile_rt);
+    }
+
+    public function testNIK()
+    {
+        // use core validation rules
+        $model = new Beneficiary();
+        $model->nik = null;
+        $model->validate();
+        $this->assertFalse($model->hasErrors('nik'));
+
+        $model->nik = ' 3211110101800001 ';
+        $model->validate();
+        $this->assertEquals('3211110101800001', $model->nik);
+
+        // use NikValidator
+        $model = new DynamicModel(['nik' => null]);
+        $model->addRule('nik', NikValidator::class);
+
+        $model->nik = '0000000101800001';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('nik'));
+
+        $model->nik = '3200000101800000';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('nik'));
+
+        $model->nik = '3200000101800001';
+        $model->validate();
+        $this->assertFalse($model->hasErrors('nik'));
+    }
+
+    public function testFamilyMembers()
+    {
+        $model = new Beneficiary();
+        $model->total_family_members = 'asd';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('total_family_members'));
+
+        $model->total_family_members = '0 1';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('total_family_members'));
+
+        $model->total_family_members = null;
+        $model->validate();
+        $this->assertFalse($model->hasErrors('total_family_members'));
+
+        $model->total_family_members = '1';
+        $model->validate();
+        $this->assertFalse($model->hasErrors('total_family_members'));
+
+        $model->total_family_members = 1;
+        $model->validate();
+        $this->assertFalse($model->hasErrors('total_family_members'));
     }
 }
