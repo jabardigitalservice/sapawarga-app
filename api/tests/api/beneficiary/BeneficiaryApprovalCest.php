efbf78db966fe208d4ed95615f9f536c0fe99469
Aldi Doanta <aldidkurnia@gmail.com>
2020-05-11 14:27:03 +0700

    [BansosApproval] Add test case for filter and reject

--- api/tests/api/beneficiary/BeneficiaryApprovalCest.php
+++ api/tests/api/beneficiary/BeneficiaryApprovalCest.php
@@ -16,7 +16,9 @@ class BeneficiaryApprovalCest
     {
         $I->haveInDatabase('beneficiaries', [
             'id' => 1,
+            'nik' => '3200000000000003',
             'domicile_kel_bps_id' => $this->kelBandung,
+            'domicile_rw' => '1',
             'status_verification' => Beneficiary::STATUS_APPROVED,
             'status' => Beneficiary::STATUS_ACTIVE,
             'name' => 'Name',
@@ -26,6 +28,19 @@ class BeneficiaryApprovalCest
 
         $I->haveInDatabase('beneficiaries', [
             'id' => 2,
+            'nik' => '3200000000000002',
+            'domicile_kel_bps_id' => $this->kelBandung,
+            'domicile_rw' => '2',
+            'status_verification' => Beneficiary::STATUS_APPROVED,
+            'status' => Beneficiary::STATUS_ACTIVE,
+            'name' => 'Name',
+            'created_at' => 0,
+            'updated_at' => 0,
+        ]);
+
+        $I->haveInDatabase('beneficiaries', [
+            'id' => 3,
+            'nik' => '3200000000000001',
             'domicile_kel_bps_id' => $this->kelBandung,
             'status_verification' => Beneficiary::STATUS_APPROVED_KEL,
             'status' => Beneficiary::STATUS_ACTIVE,
@@ -35,7 +50,7 @@ class BeneficiaryApprovalCest
         ]);
 
         $I->haveInDatabase('beneficiaries', [
-            'id' => 3,
+            'id' => 4,
             'domicile_kel_bps_id' => $this->kelBekasi,
             'status_verification' => Beneficiary::STATUS_APPROVED,
             'status' => Beneficiary::STATUS_ACTIVE,
@@ -54,11 +69,12 @@ class BeneficiaryApprovalCest
 
         $I->sendGET($this->endpointBeneficiaries);
         $I->canSeeResponseCodeIs(200);
-        $I->seeHttpHeader('X-Pagination-Total-Count', 2);
+        $I->seeHttpHeader('X-Pagination-Total-Count', 3);
 
         $data = $I->grabDataFromResponseByJsonPath('$.data.items');
-        $I->assertEquals(1, $data[0][0]['id']);
+        $I->assertEquals(3, $data[0][0]['id']);
         $I->assertEquals(2, $data[0][1]['id']);
+        $I->assertEquals(1, $data[0][2]['id']);
     }
 
     /**
@@ -68,7 +84,7 @@ class BeneficiaryApprovalCest
     {
         $I->amStaff('staffkel');
 
-        $I->sendGET("{$this->endpointBeneficiaries}?status_verification=3");
+        $I->sendGET($this->endpointBeneficiaries . '?status_verification=' . Beneficiary::STATUS_APPROVED . '&domicile_rw_like=1');
         $I->canSeeResponseCodeIs(200);
         $I->seeHttpHeader('X-Pagination-Total-Count', 1);
 
@@ -99,6 +115,32 @@ class BeneficiaryApprovalCest
         $I->seeInDatabase('beneficiaries', [
             'id' => 1,
             'status_verifcation' => Beneficiary::STATUS_APPROVED_KEL,
+        ]);
+    }
+
+    /**
+     * @before loadData
+     */
+    public function postStaffKelReject(ApiTester $I)
+    {
+        $data = [
+            'action' => Beneficiary::ACTION_REJECT,
+            'id' => [1],
+        ];
+
+        $I->amStaff('staffkel');
+
+        $I->sendPOST("{$this->endpointBeneficiaries}/approval", $data);
+        $I->canSeeResponseCodeIs(200);
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeInDatabase('beneficiaries', [
+            'id' => 1,
+            'status_verifcation' => Beneficiary::STATUS_REJECTED_KEL,
         ]);
     }
 }
