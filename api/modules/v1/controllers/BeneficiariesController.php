d975b4ba6cd949390fb5ba611fd2f10ecbb19530
Aldi Doanta <aldidkurnia@gmail.com>
2020-08-05 18:00:44 +0700

    Beneficiary: fix query for verval download

--- api/modules/v1/controllers/BeneficiariesController.php
+++ api/modules/v1/controllers/BeneficiariesController.php
@@ -916,8 +916,7 @@ class BeneficiariesController extends ActiveController
             throw new ForbiddenHttpException(Yii::t('app', 'error.role.permission'));
         };
 
-        if (
-            $action !== Beneficiary::ACTION_APPROVE &&
+        if ($action !== Beneficiary::ACTION_APPROVE &&
             $action !== Beneficiary::ACTION_REJECT
         ) {
             throw new BadRequestHttpException('Bad Request: Invalid Action');
