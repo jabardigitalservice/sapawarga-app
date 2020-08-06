fe4510344864b3b88ee3225e66c736a70c0059d2
Aldi Doanta <aldidkurnia@gmail.com>
2020-08-06 18:04:29 +0700

    Beneficiary: add model and test stubs for BeneficiaryDashboard

---
+++ api/tests/api/beneficiary/BeneficiaryDashboardCest.php
@@ -0,0 +1,15 @@
+<?php
+
+use app\models\Beneficiary;
+
+class BeneficiaryDashboardCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
