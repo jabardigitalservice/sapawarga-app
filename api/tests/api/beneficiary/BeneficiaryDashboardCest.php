a209faa79fee98ca4e1ccd785ebee3150acb135a
Aldi Doanta <aldidkurnia@gmail.com>
2020-08-07 15:06:21 +0700

    Beneficiary: create initial test case for beneficiary dashboard

--- api/tests/api/beneficiary/BeneficiaryDashboardCest.php
+++ api/tests/api/beneficiary/BeneficiaryDashboardCest.php
@@ -4,12 +4,88 @@ use app\models\Beneficiary;
 
 class BeneficiaryDashboardCest
 {
-    public function _before(ApiTester $I)
+    private $endpointDashboardSummary = '/v1/beneficiaries/dashboard-summary';
+    private $endpointDashboardList = '/v1/beneficiaries/dashboard-list';
+    private $kabkotaBandung = '3273';
+    private $kecBandung = '3273230';
+    private $kelBandung = '3273230006';
+    private $kabkotaBekasi = '3275';
+    private $kecBekasi = '3275012';
+    private $kelBekasi = '3275012003';
+
+    protected function loadData(ApiTester $I)
     {
+        $I->haveInDatabase('beneficiaries', [
+            'id' => 1,
+            'nik' => '3200000000000001',
+            'domicile_kabkota_bps_id' => $this->kabkotaBandung,
+            'domicile_kec_bps_id' => $this->kecBandung,
+            'domicile_kel_bps_id' => $this->kelBandung,
+            'domicile_rw' => '1',
+            'tahap_1_verval' => Beneficiary::STATUS_VERIFIED,
+            'status' => Beneficiary::STATUS_ACTIVE,
+            'name' => 'Name',
+            'created_at' => 0,
+            'updated_at' => 0,
+        ]);
+
+        $I->haveInDatabase('beneficiaries', [
+            'id' => 2,
+            'nik' => '3200000000000002',
+            'domicile_kabkota_bps_id' => $this->kabkotaBandung,
+            'domicile_kec_bps_id' => $this->kecBandung,
+            'domicile_kel_bps_id' => $this->kelBandung,
+            'domicile_rw' => '2',
+            'tahap_1_verval' => Beneficiary::STATUS_REJECT,
+            'status' => Beneficiary::STATUS_ACTIVE,
+            'name' => 'Name',
+            'created_at' => 0,
+            'updated_at' => 0,
+        ]);
+
+        $I->haveInDatabase('beneficiaries', [
+            'id' => 3,
+            'nik' => '3200000000000003',
+            'domicile_kabkota_bps_id' => $this->kabkotaBandung,
+            'domicile_kec_bps_id' => $this->kecBandung,
+            'domicile_kel_bps_id' => $this->kelBandung,
+            'domicile_rw' => '1',
+            'tahap_1_verval' => null,
+            'tahap_2_verval' => Beneficiary::STATUS_VERIFIED,
+            'status' => Beneficiary::STATUS_ACTIVE,
+            'name' => 'Name',
+            'created_at' => 0,
+            'updated_at' => 0,
+        ]);
+
+        $I->haveInDatabase('beneficiaries', [
+            'id' => 4,
+            'nik' => '3200000000000004',
+            'domicile_kabkota_bps_id' => $this->kabkotaBekasi,
+            'domicile_kec_bps_id' => $this->kecBekasi,
+            'domicile_kel_bps_id' => $this->kelBekasi,
+            'domicile_rw' => '1',
+            'tahap_1_verval' => Beneficiary::STATUS_VERIFIED,
+            'status' => Beneficiary::STATUS_ACTIVE,
+            'name' => 'Name',
+            'created_at' => 0,
+            'updated_at' => 0,
+        ]);
     }
 
-    // tests
-    public function tryToTest(ApiTester $I)
+     /**
+     * @before loadData
+     */
+    public function getStaffKelDashboardSummary(ApiTester $I)
     {
+        $I->amStaff('staffkel');
+
+        $I->sendGET("{$this->endpointDashboardSummary}?type=kel&code_bps={$this->kelBandung}&tahap=1");
+        $I->canSeeResponseCodeIs(200);
+
+        $data = $I->grabDataFromResponseByJsonPath('$.data');
+        $I->assertEquals(1, $data[0]['approved']);
+        $I->assertEquals(1, $data[0]['rejected']);
+        $I->assertEquals(2, $data[0]['total']);
     }
 }
