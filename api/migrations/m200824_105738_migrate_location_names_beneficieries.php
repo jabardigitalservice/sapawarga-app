f8da5793621eb5fad3bfdf07a2c8edcec32cde75
Aldi Doanta <aldidkurnia@gmail.com>
2020-08-25 14:21:11 +0700

    Beneficiary: fix syntax error in migration file

--- api/migrations/m200824_105738_migrate_location_names_beneficieries.php
+++ api/migrations/m200824_105738_migrate_location_names_beneficieries.php
@@ -18,9 +18,9 @@ class m200824_105738_migrate_location_names_beneficieries extends CustomMigratio
             INNER JOIN areas kec ON kec.code_bps = b.domicile_kec_bps_id
             INNER JOIN areas kel ON kel.code_bps = b.domicile_kel_bps_id
             SET
-            domicile_kabkota_name = kabkota.name
+            domicile_kabkota_name = kabkota.name,
             domicile_kec_name = kec.name,
-            domicile_kel_name = kel.name,
+            domicile_kel_name = kel.name
         ')->execute();
     }
 
@@ -33,9 +33,9 @@ class m200824_105738_migrate_location_names_beneficieries extends CustomMigratio
         \Yii::$app->db->createCommand('
             UPDATE beneficiaries
             SET
-            domicile_kabkota_name = NULL
+            domicile_kabkota_name = NULL,
             domicile_kec_name = NULL,
-            domicile_kel_name = NULL,
+            domicile_kel_name = NULL
         ')->execute();
     }
 }
