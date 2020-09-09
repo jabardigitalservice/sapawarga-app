360a24831cec2ef45cf35e3902a1f35ed2cfa7c7
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-09-09 03:21:33 +0000

    Bnba Monitoring: change syntax on controller parameters

--- api/modules/v1/controllers/BeneficiariesBnbaMonitoringUploadController.php
+++ api/modules/v1/controllers/BeneficiariesBnbaMonitoringUploadController.php
@@ -76,8 +76,8 @@ class BeneficiariesBnbaMonitoringUploadController extends ActiveController
     {
         $params = Yii::$app->request->getQueryParams();
 
-        $tahap_bantuan = (isset($params['tahap_bantuan'])) ? $params['tahap_bantuan'] : null;
-        $kode_kab = (isset($params['kode_kab'])) ? $params['kode_kab'] : null;
+        $tahap_bantuan = $params['tahap_bantuan'] ?? null;
+        $kode_kab = $params['kode_kab'] ?? null;
 
         BeneficiaryBnbaMonitoringUpload::updateData($tahap_bantuan, $kode_kab);
         return 'success';
