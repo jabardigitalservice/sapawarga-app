c4684d31496f9b0473b6f0de9f9380cc51538eb8
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-07-12 11:52:18 +0700

    Change env name to be exact for whatsapp only

--- api/config/components/queue.sqs.php
+++ api/config/components/queue.sqs.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => \yii\queue\sqs\Queue::class,
-    'url' => getenv('AWS_URL'), //SQS URL
+    'url' => getenv('AWS_WHATSAPP_URL'), //SQS URL
     'key' => getenv('AWS_KEY'), // SQS Key
     'secret' => getenv('AWS_SECRET'), // SQS Secret
     'region' => getenv('AWS_REGION'), // SQS Region
